b046b0e12fc716f06cd91c36415052836bbf873e
STARTRACEX <startraceebr@foxmail.com>
2025-07-10 04:46:04 +0800

    Rename LooseType

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,6 +1,6 @@
 export { default } from "tsconfig-migrate/oxc.js";
-import type { CompilerOptions as LooseType} from "tsconfig-migrate/loose-types.js";
+import type { CompilerOptions as LooseTypes } from "tsconfig-migrate/loose-types.js";
 
-export interface CompilerOptions extends LooseType {
+export interface CompilerOptions extends LooseTypes {
   customConditions?: string[];
 }
