63f9c98283a47bed9332b2be548d848949141490
Shiro Wang <startraceebr@foxmail.com>
2025-08-16 14:23:07 +0800

    Update build script

---
+++ rollup.config.ts
@@ -0,0 +1,32 @@
+import { globSync } from "node:fs";
+import type { RollupOptions } from "rollup";
+import oxc from "./src/index.ts";
+import pkg from "./package.json" with { type: "json" };
+
+export default {
+  input: Object.fromEntries(
+    globSync("src/**/*.ts").map((path) => {
+      return [path.slice(4, -3), path];
+    }),
+  ),
+  external: [
+    /^node:/,
+    ...Object.keys(pkg.dependencies).map((dep) =>
+      new RegExp(`^${dep}($|\/\.*)`)
+    ),
+  ],
+  plugins: [oxc()],
+  output: [
+    {
+      dir: "build",
+      format: "esm",
+      sourcemap: true,
+    },
+    {
+      dir: "build",
+      format: "cjs",
+      sourcemap: true,
+      entryFileNames: "[name].cjs",
+    },
+  ],
+} satisfies RollupOptions;
